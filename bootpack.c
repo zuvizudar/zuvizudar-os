d825570fb5fb28dc24b3a7b9dd17a823effbd0da
zuvizudar <zuvizudar@yahoo.co.jp>
2017-09-08 20:13:31 +0900

    8_day

--- bootpack.c
+++ bootpack.c
@@ -1,12 +1,21 @@
 #include<stdio.h>
 #include"bootpack.h"
-void enable_mouse(void);
-void init_keyboard(void);
+
+typedef struct{
+	unsigned char buf[3],phase;
+	int x,y,btn;
+}MOUSE_DEC;
+
 extern FIFO8 keyfifo,mousefifo;
+void enable_mouse(MOUSE_DEC *mdec);
+void init_keyboard(void);
+int mouse_decode(MOUSE_DEC *mdec,unsigned char dat);
+
 void ZuviMain(void){
 	BOOTINFO *boot_info=(BOOTINFO*)0x0ff0;
 	char s[101],mccursor[256],keybuf[32],mousebuf[128];
 	int mx,my,i;
+	MOUSE_DEC mdec;
 
 	init_gdtidt();
 	init_pic();
@@ -23,17 +32,9 @@ void ZuviMain(void){
 	mx=(boot_info->screen_x-16)/2;
 	my=(boot_info->screen_y-28-16)/2;
 	init_mouse_cursor8(mccursor,COL8_840084);
-
 	putblock8_8(boot_info->vram,boot_info->screen_x,16,16,mx,my,mccursor,16);
-/*
-	putfonts8_asc(boot_info->vram,boot_info->screen_x,8,8,COL8_FFFFFF,"ABC 123");
-	putfonts8_asc(boot_info->vram,boot_info->screen_x,31,31,COL8_000000,"Zuvizudar OS");
-	putfonts8_asc(boot_info->vram,boot_info->screen_x,30,30,COL8_FFFFFF,"Zuvizudar OS");
 
-	sprintf(s,"screen_x=%d",boot_info->screen_x);
-	putfonts8_asc(boot_info->vram,boot_info->screen_x,16,64,COL8_FFFFFF,s);
-	*/
-	enable_mouse();
+	enable_mouse(&mdec);
 	for (;;) {
 		io_cli();
 		if(fifo8_status(&keyfifo)+fifo8_status(&mousefifo)==0){
@@ -50,10 +51,40 @@ void ZuviMain(void){
 			else if(fifo8_status(&mousefifo)!=0){
 				i=fifo8_get(&mousefifo);
 				io_sti();
-				sprintf(s,"%02x",i);
-				boxfill8(boot_info->vram,boot_info->screen_x,COL8_840084,0,16,47,31);
-				putfonts8_asc(boot_info->vram,boot_info->screen_x,32,16,COL8_FFFFFF,s);
+				//マウスのデータは3byteずつ
+				if (mouse_decode(&mdec,i)!= 0) {
+					sprintf(s, "[lcr %4d %4d]",mdec.x,mdec.y);
+					if((mdec.btn&0x01)!=0){
+						s[1]='L';
+					}
+					if((mdec.btn&0x02)!=0){
+						s[3]='R';
+					}
+					if((mdec.btn&0x04)!=0){
+						s[2]='C';
+					}
+					boxfill8(boot_info->vram, boot_info->screen_x, COL8_840084,32,16,32+15* 8-1,31);
+					putfonts8_asc(boot_info->vram, boot_info->screen_x,32,16,COL8_FFFFFF,s);
 
+					//マウス移動
+					boxfill8(boot_info->vram,boot_info->screen_x,COL8_840084,mx,my,mx+15,my+15);
+					mx+=mdec.x;
+					my+=mdec.y;
+					if(mx<0)
+						mx=0;
+					if(my<0)
+						my=0;
+					if(mx>boot_info->screen_x-16)
+						mx=boot_info->screen_x-16;
+					if(my>boot_info->screen_y-16)
+						mx=boot_info->screen_y-16;
+					
+					sprintf(s,"(%3d,%3d)",mx,my);
+					boxfill8(boot_info->vram,boot_info->screen_x,COL8_840084,0,0,79,15);
+					putfonts8_asc(boot_info->vram,boot_info->screen_x,0,0,COL8_FFFFFF,s);
+					putblock8_8(boot_info->vram,boot_info->screen_x,16,16,mx,my,mccursor,16);
+
+				}
 			}
 		}
 	}
@@ -86,10 +117,48 @@ void init_keyboard(void){
 #define KEYCMD_SENDTO_MOUSE		0xd4
 #define MOUSECMD_ENABLE			0xf4
 
-void enable_mouse(void){
+void enable_mouse(MOUSE_DEC *mdec){
 	wait_keyboard_controller_sendready();
 	io_out8(PORT_KEYCMD,KEYCMD_SENDTO_MOUSE);
 	wait_keyboard_controller_sendready();
 	io_out8(PORT_KEYDAT,MOUSECMD_ENABLE);
+	mdec->phase=0;
 	return;
+}
+
+int mouse_decode(MOUSE_DEC *mdec, unsigned char dat){
+	if (mdec->phase == 0) {
+			if (dat == 0xfa) {
+						mdec->phase = 1;
+					}
+			return 0;
+		}
+	if (mdec->phase == 1) {
+		if((dat&0xc8)==0x08){
+			mdec->buf[0] = dat; //クリック、移動
+			mdec->phase = 2;
+		}
+			return 0;
+		}
+	if (mdec->phase == 2) {
+			mdec->buf[1] = dat; //左右
+			mdec->phase = 3;
+			return 0;
+		}
+	if (mdec->phase == 3) {
+			mdec->buf[2] = dat; //上下
+			mdec->phase = 1;
+			mdec->btn=mdec->buf[0]& 0x07;
+			mdec->x=mdec->buf[1];
+			mdec->y=mdec->buf[2];
+			if((mdec->buf[0]&0x10)!=0){
+				mdec->x|=0xffffff00;
+			}
+			if((mdec->buf[0]&0x20)!=0){
+				mdec->y|=0xffffff00;
+			}
+			mdec->y=-mdec->y;
+			return 1;
+		}
+	return -1;
 }
