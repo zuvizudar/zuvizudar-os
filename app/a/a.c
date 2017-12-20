9b102ae2d5d9397c83c61b9d03ce7c660aa2660c
zuvizudar <zuvizudar@yahoo.co.jp>
2017-12-21 02:07:39 +0900

    change name

---
+++ app/a/a.c
@@ -0,0 +1,7 @@
+void api_putchar(int c);
+void api_end(void);
+
+void ZuviMain(void){
+	api_putchar('A');
+	api_end();
+}
---
+++ app/log/day-25-h.c
@@ -0,0 +1,366 @@
+#include<stdio.h>
+#include<string.h>
+#include"bootpack.h"
+
+#define KEYCMD_LED 0xed
+int keywin_off(SHEET *key_win,SHEET *sht_win, int cur_c, int cur_x);
+int keywin_on(SHEET *key_win, SHEET *sht_win, int cur_c);
+void ZuviMain(void){
+
+	BOOTINFO *boot_info=(BOOTINFO*)0x0ff0;
+	FIFO32 fifo,keycmd;
+	char s[40];
+	int fifobuf[128],keycmd_buf[32];
+	int mx,my,i,cursor_x,cursor_c;//count=0;
+	unsigned int memtotal;
+	MOUSE_DEC mdec;
+	MEMMAN *memman=(MEMMAN*)MEMMAN_ADDR;
+	SHTCTL *shtctl;
+	static char keytable0[0x80] = {
+		0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '^', 0,   0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '@', '[', 0,   0,   'A', 'S',
+		'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', ':', 0,   0,   ']', 'Z', 'X', 'C', 'V',
+		'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+		'2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0x5c, 0,  0,   0,   0,   0,   0,   0,   0,   0,   0x5c, 0,  
+	};
+	static char keytable1[0x80] = {
+		0,   0,   '!', 0x22, '#', '$', '%', '&', 0x27, '(', ')', '~', '=', '~', 0,   0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '`', '{', 0,   0,   'A', 'S',
+		'D', 'F', 'G', 'H', 'J', 'K', 'L', '+', '*', 0,   0,   '}', 'Z', 'X', 'C', 'V',
+		'B', 'N', 'M', '<', '>', '?', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+		'2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   '_', 0,   0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0
+	};
+	unsigned char *buf_back,buf_mouse[256],*buf_win,*buf_cons[2];
+	SHEET *sht_back,*sht_mouse,*sht_win,*sht_cons[2];
+	TASK *task_a,*task_cons[2],*task;
+	TIMER *timer;
+	int key_shift=0,key_leds=(boot_info->leds >> 4)&7,keycmd_wait=1;
+	CONSOLE *cons;
+	int j,x,y,mmx=-1,mmy=-1;
+	SHEET *sht=0,*key_win;
+
+	init_gdtidt();
+	init_pic();
+	io_sti();
+	fifo32_init(&fifo,32,fifobuf,0);
+	init_pit();
+	init_keyboard(&fifo,256);
+	enable_mouse(&fifo,512,&mdec);
+	io_out8(PIC0_IMR,0xf8);//キーボード
+	io_out8(PIC1_IMR,0xef);//マウス
+	fifo32_init(&keycmd,32,keycmd_buf,0);
+
+	memtotal=memtest(0x00400000,0xbfffffff);
+	memman_init(memman);
+	memman_free(memman,0x00001000,0x0009e000);
+	memman_free(memman,0x00400000,memtotal-0x00400000);
+
+	init_palette();
+	shtctl=shtctl_init(memman,boot_info->vram,boot_info->screen_x,boot_info->screen_y);
+	task_a=task_init(memman);
+	fifo.task=task_a;
+	task_run(task_a,1,2);
+	*((int *)0x0fe4) = (int) shtctl;
+	//back
+	sht_back=sheet_alloc(shtctl);
+	buf_back=(unsigned char *)memman_alloc_4k(memman,boot_info->screen_x*boot_info->screen_y);
+	sheet_setbuf(sht_back,buf_back,boot_info->screen_x,boot_info->screen_y,-1);
+	init_screen(buf_back,boot_info->screen_x,boot_info->screen_y);
+
+	//console
+	for(i=0;i<2;i++){
+		sht_cons[i] = sheet_alloc(shtctl);
+		buf_cons[i] = (unsigned char *) memman_alloc_4k(memman, 256 * 165);
+		sheet_setbuf(sht_cons[i], buf_cons[i], 256, 165, -1);
+		make_window8(buf_cons[i], 256, 165, "console", 0);
+		make_textbox8(sht_cons[i], 8, 28, 240, 128, COL8_000000);
+		task_cons[i] = task_alloc();
+		task_cons[i]->tss.esp = memman_alloc_4k(memman, 64 * 1024) + 64 * 1024 - 12;
+		task_cons[i]->tss.eip = (int) &console_task;
+		task_cons[i]->tss.es = 1 * 8;
+		task_cons[i]->tss.cs = 2 * 8;
+		task_cons[i]->tss.ss = 1 * 8;
+		task_cons[i]->tss.ds = 1 * 8;
+		task_cons[i]->tss.fs = 1 * 8;
+		task_cons[i]->tss.gs = 1 * 8;
+		*((int *) (task_cons[i]->tss.esp + 4)) = (int) sht_cons[i];
+		*((int *) (task_cons[i]->tss.esp +8 )) =memtotal;
+		task_run(task_cons[i], 2, 2);
+		sht_cons[i]->task = task_cons[i];
+		sht_cons[i]->flags |= 0x20;  
+	}
+	//win
+	sht_win=sheet_alloc(shtctl);
+	buf_win=(unsigned char *)memman_alloc_4k(memman,144*52);
+	sheet_setbuf(sht_win,buf_win,144,52,-1);
+  make_window8(buf_win,144,52,"task_a",1);
+	make_textbox8(sht_win,8,28,128,16,COL8_FFFFFF);
+	cursor_x=8;
+	cursor_c=COL8_FFFFFF;
+	timer=timer_alloc();
+	timer_init(timer,&fifo,1);
+	timer_settime(timer,50);
+	
+	//mouse
+	sht_mouse=sheet_alloc(shtctl);
+	sheet_setbuf(sht_mouse,buf_mouse,16,16,99);
+	init_mouse_cursor8(buf_mouse,99);
+	mx=(boot_info->screen_x-16)/2;
+	my=(boot_info->screen_y-28-16)/2;
+
+	sheet_slide(sht_back,0,0);
+	sheet_slide(sht_mouse,mx,my);
+	sheet_slide(sht_win,8,56);
+	sheet_slide(sht_cons[1],56,6);
+	sheet_slide(sht_cons[0],8,2);
+
+	sheet_updown(sht_back,0);
+	sheet_updown(sht_cons[0],1);
+	sheet_updown(sht_cons[1],2);
+	sheet_updown(sht_win,3);
+	sheet_updown(sht_mouse,4);
+	key_win = sht_win;
+
+	fifo32_put(&keycmd,KEYCMD_LED);
+	fifo32_put(&keycmd,key_leds);
+	for (;;) {
+		if (fifo32_status(&keycmd) > 0 && keycmd_wait < 0) {
+			keycmd_wait = fifo32_get(&keycmd);
+			wait_keyboard_controller_sendready();
+			io_out8(PORT_KEYDAT, keycmd_wait);
+		}
+		io_cli();
+		if(fifo32_status(&fifo)==0){
+			task_sleep(task_a);
+			io_sti();
+		}
+		else {
+			i=fifo32_get(&fifo);
+			io_sti();
+			if(key_win->flags == 0){
+				key_win = shtctl->sheets[shtctl->top -1];
+				cursor_c = keywin_on(key_win,sht_win,cursor_c);
+			}
+			if(256<=i&&i<=511){
+				if (i < 0x80 + 256) {
+					if (key_shift==0){
+							s[0] = keytable0[i - 256];
+					}
+					else{
+							s[0] = keytable1[i - 256];
+					}
+				}
+				else{
+					s[0]=0;
+				}
+				if ('A' <= s[0] && s[0] <= 'Z') {
+					if (((key_leds & 4) == 0 && key_shift == 0) ||((key_leds & 4) != 0 && key_shift != 0)){ 
+						s[0] += 0x20;
+					}
+				}
+				if(s[0]!=0){
+					if(key_win == sht_win){
+						if(cursor_x<128){
+							s[1]=0;
+							putfonts8_asc_sht(sht_win,cursor_x,28,COL8_000000,COL8_FFFFFF,s,1);
+							cursor_x+=8;
+						}
+					}
+					else{
+						fifo32_put(&key_win->task->fifo,s[0]+256);
+					}
+				}
+				if(i == 256 + 0x0e) {//バックスペース
+					if (key_win == sht_win) {
+						if (cursor_x > 8) {
+							putfonts8_asc_sht(sht_win, cursor_x, 28, COL8_000000, COL8_FFFFFF, " ", 1);
+							cursor_x -= 8;
+						}
+					} else {
+						fifo32_put(&key_win->task->fifo, 8 + 256);
+					}
+				}
+				if(i == 256+0x1c){ //enter
+					if(key_win != sht_win){
+						fifo32_put(&key_win->task->fifo,10+256);
+					}
+				}
+				if (i == 256 + 0x0f) { //tab
+						cursor_c = keywin_off(key_win,sht_win,cursor_c,cursor_x);
+						j = key_win->height-1;
+						if(j==0){
+							j = shtctl->top-1;
+						}
+						key_win = shtctl->sheets[j];
+						cursor_c = keywin_on(key_win,sht_win,cursor_c);
+					} 
+				if (i == 256 + 0x2a){
+					key_shift |= 1;
+				}
+				if (i == 256 + 0x36) {
+					key_shift |= 2;
+				}
+				if (i == 256 + 0xaa) {
+					key_shift &= ~1;
+				}
+				if (i == 256 + 0xb6) {
+					key_shift &= ~2;
+				}
+				if (i == 256 + 0x3a) {
+					key_leds ^= 4;
+					fifo32_put(&keycmd, KEYCMD_LED);
+					fifo32_put(&keycmd, key_leds);
+				}
+				if (i == 256 + 0x45) {
+					key_leds ^= 2;
+					fifo32_put(&keycmd, KEYCMD_LED);
+					fifo32_put(&keycmd, key_leds);
+				}
+				if (i == 256 + 0x46) {
+					key_leds ^= 1;
+					fifo32_put(&keycmd, KEYCMD_LED);
+					fifo32_put(&keycmd, key_leds);
+				}
+				if (i ==256 +0x3b && key_shift != 0){
+					//shift+f1
+					task=key_win->task;
+					if(task!=0 && task->tss.ss0 != 0){
+						cons_putstr0(task->cons,"\nBreak(key):\n");
+						io_cli();
+						task->tss.eax=(int)&(task->tss.esp0);
+						task->tss.eip=(int)asm_end_app;
+					io_sti();
+					}
+				}
+				if(i==256+0x57){
+					sheet_updown(shtctl->sheets[1],shtctl->top);
+				}
+				if (i == 256 + 0xfa) {
+					keycmd_wait = -1;
+				}
+				if (i == 256 + 0xfe) {
+					wait_keyboard_controller_sendready();
+					io_out8(PORT_KEYDAT, keycmd_wait);
+				}
+				if(cursor_c<=0){
+					boxfill8(sht_win->buf,sht_win->bxsize,cursor_c,cursor_x,28,cursor_x+7,43);
+				}
+				boxfill8(sht_win->buf,sht_win->bxsize,cursor_c,cursor_x,28,cursor_x+7,43);
+				sheet_refresh(sht_win,cursor_x,28,cursor_x+8,44);
+			}
+
+			else if(512<=i&&i<=767){
+				//マウスのデータは3byteずつ
+				if (mouse_decode(&mdec,i-512)!= 0) {
+					mx+=mdec.x;
+					my+=mdec.y;
+					if(mx<0)
+						mx=0;
+					if(my<0)
+						my=0;
+					if(mx>boot_info->screen_x-1)
+						mx=boot_info->screen_x-1;
+					if(my>boot_info->screen_y-1)
+						mx=boot_info->screen_y-1;
+					
+				sheet_slide(sht_mouse,mx,my);
+				if ((mdec.btn & 0x01) != 0) {
+						if (mmx < 0) {
+							for (j = shtctl->top - 1; j > 0; j--) {
+								sht = shtctl->sheets[j];
+								x = mx - sht->vx0;
+								y = my - sht->vy0;
+								if (0 <= x && x < sht->bxsize && 0 <= y && y < sht->bysize) {
+									if (sht->buf[y * sht->bxsize + x] != sht->col_inv) {
+										sheet_updown(sht, shtctl->top - 1);
+										if (sht != key_win) {
+											cursor_c = keywin_off(key_win, sht_win, cursor_c, cursor_x);
+											key_win = sht;
+											cursor_c = keywin_on(key_win, sht_win, cursor_c);
+										}
+										if (3 <= x && x < sht->bxsize - 3 && 3 <= y && y < 21) {
+											mmx = mx;
+											mmy = my;
+										}
+										if (sht->bxsize - 21 <= x && x < sht->bxsize - 5 && 5 <= y && y < 19) {
+											//xクリック
+											if ((sht->flags & 0x10) != 0) {	
+												task=sht->task;
+												cons_putstr0(task->cons,"\nBreak(mouse):\n");
+												io_cli();	
+												task->tss.eax = (int) &(task->tss.esp0);
+												task->tss.eip = (int) asm_end_app;
+												io_sti();
+											}
+										}
+										break;
+									}
+								}
+							}
+						}
+						else {
+							x = mx - mmx;
+							y = my - mmy;
+							sheet_slide(sht, sht->vx0 + x, sht->vy0 + y);
+							mmx = mx;
+							mmy = my;
+						}
+					} 
+					else {
+						mmx = -1;
+					}
+				}
+			}
+			else if(i<=1){
+				if(i!=0){
+					timer_init(timer,&fifo,0);
+					if(cursor_c>=0){
+						cursor_c=COL8_000000;
+					}
+				}
+				else{
+					timer_init(timer,&fifo,1);
+					if(cursor_c>=0){
+						cursor_c=COL8_FFFFFF;
+					}
+				}
+				timer_settime(timer,50);
+				if(cursor_c>=0){
+					boxfill8(sht_win->buf,sht_win->bxsize,cursor_c,cursor_x,28,cursor_x+7,43);
+					sheet_refresh(sht_win,cursor_x,28,cursor_x+8,44);
+				}
+			}
+		}
+	}
+}
+
+int keywin_off(SHEET *key_win,SHEET *sht_win, int cur_c, int cur_x){
+	change_wtitle8(key_win, 0);
+	if (key_win == sht_win) {
+		cur_c = -1;
+		boxfill8(sht_win->buf, sht_win->bxsize, COL8_FFFFFF, cur_x, 28, cur_x + 7, 43);
+	} else {
+		if ((key_win->flags & 0x20) != 0) {
+			fifo32_put(&key_win->task->fifo, 3);
+		}
+	}
+	return cur_c;
+}
+
+int keywin_on(SHEET *key_win,SHEET *sht_win, int cur_c){
+	change_wtitle8(key_win, 1);
+	if (key_win == sht_win) {
+		cur_c = COL8_000000;
+	} else {
+		if ((key_win->flags & 0x20) != 0) {
+			fifo32_put(&key_win->task->fifo, 2);
+		}
+	}
+	return cur_c;
+}
